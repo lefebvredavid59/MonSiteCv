513d08fd24a3634ad51f2de38bddd9fe28eb9604
lefebvredavid59 <dlefebvredev@gmail.com>
2020-02-23 20:26:21 +0100

    resolve error image not null

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Bridge\Mailgun\Transport\MailgunSmtpTransport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -12,8 +19,36 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils,
+                          MailerInterface $mailer,
+                          Request $request): Response
     {
+
+        $transport = new MailgunSmtpTransport('', '');
+        $mailer = new Mailer($transport);
+
+        $contact = new Contact();
+        $contactForm = $this->createForm(ContactType::class,$contact);
+        $contactForm->handleRequest($request);
+
+        if ($contactForm->isSubmitted() && $contactForm->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            $email = (new Email())
+                ->from($contact->getEmail())
+                ->to('dlefebvredev@gmail.com')
+                ->subject($contact->getRaison())
+//                ->htmlTemplate('mail/confirmMail.html.twig')
+                ->text($contact->getMessage());
+
+            $mailer->send($email);
+
+            $entityManager->remove($contact);
+            $entityManager->flush();
+        }
+
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
         // }
@@ -23,7 +58,11 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+            'contact' => $contactForm->createView(),
+            ]);
     }
 
     /**
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -57,7 +57,7 @@ class Article
     private $categorie;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      * @var string
      */
     private $image;
@@ -69,7 +69,7 @@ class Article
     private $imageFile;
 
     /**
-     * @ORM\Column(type="datetime")
+     * @ORM\Column(type="datetime", nullable=true)
      * @var \DateTime
      */
     private $updatedAt;
